069653b74775263faaba923cd2417fef570798f5
TigerKao <86467281+TigerKao@users.noreply.github.com>
2022-05-24 14:53:52 +0800

    photo adjust

--- src/components/SocialItem/index.js
+++ src/components/SocialItem/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../constants/colors';
 import { respondTo } from '../../utils/responsive';
-import { Twitter, Facebook, Discord, Opensea } from '../../components/Icons';
+import { Twitter, Instagram, Discord, Opensea } from '../../components/Icons';
 
 const SocialItem = ({icon, children, ...props}) => {
   return (
     <Item {...props}>
       { children }
       { icon === 'Twitter' && <Twitter /> }
-      { icon === 'Facebook' && <Facebook /> }
+      { icon === 'Instagram' && <Instagram /> }
       { icon === 'Discord' && <Discord /> }
       { icon === 'Opensea' && <Opensea /> }
     </Item>
